386daa0d184015bbf4535727419ec39209bf9a9e
FerasTB <al2mohtrf@gmail.com>
2024-09-16 19:07:56 +0300

    first try 3

--- routes/api.php
+++ routes/api.php
@@ -22,17 +22,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->get('/trips', [TripController::class, 'index']);
 
 Route::prefix('users')->group(function () {
     Route::post('register', [UserController::class, 'register']);
     Route::post('login', [UserController::class, 'login']);
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/trips/{trip}/seats', [BookingController::class, 'getSeats']);
-        Route::get('/trips/{trip}/seats/admin', [AdminController::class, 'getSeats']);
         Route::post('/trips/{trip}/seats/book', [BookingController::class, 'bookSeats']);
         Route::put('/trips/{trip}/seats/{booking}/update', [BookingController::class, 'updateSeat']);
         Route::get('/user/bookings/pending', [BookingController::class, 'getPendingBookings']);
-        Route::apiResource('trips', TripController::class);
         Route::post('/bookings/approve', [AdminController::class, 'approveBookings']);
         Route::post('/bookings/reject', [AdminController::class, 'rejectBookings']);
         Route::post('/register/admin', [AdminController::class, 'registerWithoutPassword']);
@@ -44,7 +43,19 @@ Route::prefix('users')->group(function () {
     });
 });
 
-Route::prefix('admin')->middleware(['auth:sanctum', 'is_admin'])->group(function () {
+Route::prefix('admin')->middleware(['auth:sanctum', 'admin'])->group(function () {
+    // Create a new trip
+    Route::post('/trips', [TripController::class, 'store']);
+
+    // Get a specific trip by ID
+    Route::get('/trips/{trip}', [TripController::class, 'show']);
+
+    // Update a specific trip by ID
+    Route::put('/trips/{trip}', [TripController::class, 'update']);
+
+    // Delete a specific trip by ID
+    Route::delete('/trips/{trip}', [TripController::class, 'destroy']);
+    Route::get('/trips/{trip}/seats/admin', [AdminController::class, 'getSeats']);
     Route::get('users', [AdminController::class, 'index']);
     Route::get('users/{user}', [AdminController::class, 'show']);
     Route::put('users/{user}', [AdminController::class, 'update']);
